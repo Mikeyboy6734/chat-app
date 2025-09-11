240938932077e36d479dd228072107c6458e851a
Mikeyboy6734 <Mikeygett@outlook.com>
2025-09-11 17:11:24 -0400

    Create server.js

---
+++ chat-app/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.use(express.static(__dirname)); // Serve index.html
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
